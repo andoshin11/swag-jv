7a6a285b9ab1796b2868705d272e5b957b7f11bf
andoshin11 <shinglish11@gmail.com>
2019-05-17 21:41:56 +0900

    Generate paths validators

--- example/test.js
+++ example/test.js
@@ -1,4 +1,5 @@
 const schemaValidators = require('./validators/schema')
+const pathsValidators = require('./validators/paths')
 
 const correctData = {
   id: 1,
@@ -17,3 +18,12 @@ console.log(schemaValidators.PetValidator(correctData))
 // }
 
 // console.log(schemaValidators.PetValidator(inCorrectData))
+
+const corretResponse = {
+  total: 1,
+  per_page: 5,
+  page: 1,
+  pets: [],
+}
+
+console.log(pathsValidators.GetPetsValidator(corretResponse))
